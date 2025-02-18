2657d04cf91c30a78c0b8d4043a2eba83f2767a7
iassam <iss.iassam@gmail.com>
2025-02-18 02:28:51 -0300

    doc: add documentation of shared helpers

--- src/user/shared/helpers/object.helper.ts
+++ src/user/shared/helpers/object.helper.ts
@@ -1,3 +1,8 @@
+/**
+ * Removes null or undefined attributes from an object.
+ * @param obj - The object to process.
+ * @returns Object without null attributes.
+ */
 export const removeNullAttributes = (obj: any): any =>  {
     return Object.fromEntries(
       Object.entries(obj).filter(([_, v]) => v != null)
--- src/user/shared/helpers/password.helper.ts
+++ src/user/shared/helpers/password.helper.ts
@@ -1,5 +1,10 @@
 import * as bcrypt from "bcrypt";
 
+/**
+ * Encrypts a password using bcrypt.
+ * @param password - The password to encrypt.
+ * @returns The encrypted password.
+ */
 const encPassword = async (password: string): Promise<string> => {
   const salt = await bcrypt.genSalt(10);
   return bcrypt.hash(password, salt);
