5776bc78ebc7999ae403cac191c246a8641dd221
iassam <iss.iassam@gmail.com>
2025-02-18 02:20:50 -0300

    doc: add documentation of entities

--- src/user/application/entities/user.entity.ts
+++ src/user/application/entities/user.entity.ts
@@ -1,25 +1,49 @@
 import { Exclude, Expose } from "class-transformer";
 import { UserRole } from "../../shared/enums";
 
+/**
+ * Entity representing a user.
+ */
 export class UserEntity {
+  /**
+   * The ID of the user.
+   */
   @Expose()
   id?: string;
 
+  /**
+   * The name of the user.
+   */
   @Expose()
   name: string;
 
+  /**
+   * The email of the user.
+   */
   @Expose()
   email: string;
 
+  /**
+   * The password of the user.
+   */
   @Exclude()
   password: string;
 
+  /**
+   * The role of the user.
+   */
   @Expose()
   role: UserRole;
 
+  /**
+   * The date and time when the user was created.
+   */
   @Expose()
   created_at?: Date;
 
+  /**
+   * The date and time when the user was last updated.
+   */
   @Expose()
   updated_at?: Date;
 }
